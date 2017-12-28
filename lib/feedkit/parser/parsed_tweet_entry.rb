d26b0635bd5670b47298a492d038667e9dfae529
Ben Ubois <ben@benubois.com>
2017-12-27 20:33:01 -0800

    Tests.

--- lib/feedkit/parser/parsed_tweet_entry.rb
+++ lib/feedkit/parser/parsed_tweet_entry.rb
@@ -1,55 +1,55 @@
 module Feedkit
   module Parser
 
-class ParsedTweetEntry < ParsedEntry
+    class ParsedTweetEntry < ParsedEntry
 
-  def initialize(tweet, feed_url)
-    @tweet = tweet
-    @feed_url = feed_url
-  end
+      def initialize(tweet, feed_url)
+        @tweet = tweet
+        @feed_url = feed_url
+      end
 
-  def entry_id
-    @tweet.id
-  end
+      def entry_id
+        @tweet.id
+      end
 
-  def author
-    main_tweet.user.name || "@#{main_tweet.user.screen_name}"
-  end
+      def author
+        main_tweet.user.name || "@#{main_tweet.user.screen_name}"
+      end
 
-  def content
-    if tweet_hash[:entities]
-      Twitter::TwitterText::Autolink.auto_link_with_json(tweet_hash[:full_text], tweet_hash[:entities])
-    else
-      tweet_hash[:full_text]
+      def content
+        if tweet_hash[:entities]
+          Twitter::TwitterText::Autolink.auto_link_with_json(tweet_hash[:full_text], tweet_hash[:entities])
+        else
+          tweet_hash[:full_text]
+        end
+      end
+
+      def data
+        value = {}
+        value["tweet"] = @tweet.to_h
+        value
+      end
+
+      def published
+        @tweet.created_at
+      end
+
+      def title
+        "#{main_tweet.user.name} @#{main_tweet.user.screen_name}"
+      end
+
+      def url
+        @tweet.url.to_s
+      end
+
+      def main_tweet
+        (@tweet.retweeted_status?) ? @tweet.retweeted_status : @tweet
+      end
+
+      def tweet_hash
+        @tweet_hash ||= main_tweet.to_h
+      end
+
     end
   end
-
-  def data
-    value = {}
-    value["tweet"] = @tweet.to_h
-    value
-  end
-
-  def published
-    @tweet.created_at
-  end
-
-  def title
-    "#{main_tweet.user.name} @#{main_tweet.user.screen_name}"
-  end
-
-  def url
-    @tweet.url.to_s
-  end
-
-  def main_tweet
-    (@tweet.retweeted_status?) ? @tweet.retweeted_status : @tweet
-  end
-
-  def tweet_hash
-    @tweet_hash ||= main_tweet.to_h
-  end
-
-end
-end
 end
--- test/feedkit/parser/twitter_feed_test.rb
+++ test/feedkit/parser/twitter_feed_test.rb
@@ -8,7 +8,7 @@ class Feedkit::TwitterFeedTest < Minitest::Test
     end
 
     def user(*args)
-      []
+      {name: "name"}
     end
 
     def home_timeline(*args)
@@ -25,24 +25,49 @@ class Feedkit::TwitterFeedTest < Minitest::Test
   end
 
   def test_should_load_tweets
-    urls = [
-      "https://twitter.com/bsaid",
-      "@bsaid",
-      "https://twitter.com/search?q=feedbin+ben&src=typd",
-      "https://twitter.com/bsaid/lists/conversationlist",
-      "https://twitter.com/hashtag/feedbin?src=hash",
-      "#feedbin",
-    ]
+    urls = {
+      "https://twitter.com/bsaid"                         => "https://twitter.com/bsaid",
+      "@bsaid"                                            => "https://twitter.com/bsaid",
+      "https://twitter.com/search?q=feedbin+ben&src=typd" => "https://twitter.com/search?q=feedbin+ben&src=typd",
+      "https://twitter.com/bsaid/lists/conversationlist"  => "https://twitter.com/bsaid/lists/conversationlist",
+      "https://twitter.com/hashtag/feedbin?src=hash"      => "https://twitter.com/hashtag/feedbin?src=hash",
+      "#feedbin"                                          => "https://twitter.com/hashtag/feedbin",
+      "twitter.com"                                       => "https://twitter.com?screen_name=bsaid",
+    }
 
-    urls.each do |url|
-      url = ::Feedkit::TwitterURLRecognizer.new(url, "bsaid")
+    urls.each do |input_url, output_url|
+      url = ::Feedkit::TwitterURLRecognizer.new(input_url, "bsaid")
 
       feed = Feedkit::TwitterFeed.new(url, 'asdf', 'asdf')
       feed.stub :client, TwitterClient.new do
-        feed.feed
+        assert_equal(output_url, feed.feed.feed_url)
+        assert !!feed.feed.title
       end
-
     end
+  end
+
+  def test_should_have_feed_properties
+    input_url = "https://twitter.com/bsaid"
+
+    url = ::Feedkit::TwitterURLRecognizer.new(input_url, "bsaid")
+
+    feed = Feedkit::TwitterFeed.new(url, 'asdf', 'asdf')
+    feed.stub :client, TwitterClient.new do
+      assert !!feed.feed.entries
+
+      to_feed = {
+        etag: nil,
+        feed_url: "https://twitter.com/bsaid",
+        last_modified: nil,
+        self_url: "https://twitter.com/bsaid",
+        site_url: "https://twitter.com/bsaid",
+        title: "@bsaid",
+        feed_type: :twitter,
+        options: { "twitter_user"=>{name: "name"} }
+      }
+      assert_equal(to_feed, feed.feed.to_feed)
+    end
+
   end
 
 end
